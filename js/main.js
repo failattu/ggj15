3487883833ca3f3e62d1e080e572a57003c8e963
ksirkesalo <kalle.sirkesalo@eficode.com>
2015-01-24 13:34:54 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+enchant();
+ 
+window.onload = function() {
+	var game = new Game(320, 320);
+	game.fps = 60;
+	game.preload('assets/chara1.png', 'assets/ground.png', 'assets/cannon.png', 'assets/bullet.png');
+	game.onload = function () {
+
+		initWorld();
+
+		// var scene = new Scene();
+		// game.pushScene(scene);
+		
+		// var bear = new Sprite(32, 32);
+		// bear.image = game.assets['assets/chara1.png'];
+
+  //       bear.x = 0;
+  //       bear.y = 0;
+
+  //       bear.baseFrame = 5;
+  //       bear.frame = bear.baseFrame;
+
+		// scene.addChild(bear);
+
+  //       bear.addEventListener("enterframe", function(){
+  //           this.x += 1;
+  //           this.frame = this.age % 2 + this.baseFrame;
+  //       });
+
+  //       bear.addEventListener("touchstart", function(){
+  //           // scene.removeChild(bear);
+  //           this.baseFrame += 1;
+  //       });
+
+	};
+
+	function Player() {
+		this.fireCooldown = 0;
+		this.resources = 10;
+	}
+
+	function initWorld() {
+		var gameScene = new Scene();
+		game.pushScene(gameScene);
+
+		var p1 = new Player();
+		var p2 = new Player();
+
+	    var p1_cannon = new Sprite(100,100);
+	    p1_cannon.image = game.assets['assets/cannon.png'];
+
+	    p1_cannon.x = 30;
+	    p1_cannon.y = 248;
+
+	    gameScene.addChild(p1_cannon);
+
+	    p1_cannon.addEventListener("enterframe", function(){
+	        if (game.input.left && !game.input.right) {
+				this.rotation -= 1.5;
+			}
+	        if (game.input.right && !game.input.left) {
+				this.rotation += 1.5;
+			}
+	    });
+		
+		var ground = new Sprite(320, 32);
+		ground.image = game.assets['assets/ground.png'];
+
+	    ground.x = 0;
+	    ground.y = 298;
+
+	    gameScene.addChild(ground);
+
+	    gameScene.addEventListener("enterframe", function() {
+	        if (game.input.up) {
+				fire(p1, 50, 220);
+			}
+			p1.fireCooldown -= 1;
+	    });
+	}
+
+	function fire(player, startX, startY) {
+	    if (player.fireCooldown <= 0 && player.resources != 0) {
+		    var bullet = new Sprite(16, 16);
+		    bullet.x = startX;
+		    bullet.y = startY;
+		    bullet.image = game.assets['assets/bullet.png'];
+
+		    bullet.addEventListener('enterframe', function(e) {
+		        this.y -= 6;
+		    });
+		    game.currentScene.addChild(bullet);
+		    // console.log("bullet fired.");
+		    player.fireCooldown = 20;
+		    player.resources -= 1;
+		}
+	}
+	game.start();
+}
