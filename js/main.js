d1604f201764d1d2a102a80b663caec366f6c4ac
Metsaroll <deemu.h@gmail.com>
2015-01-24 20:17:48 +0200

    Sound patch

--- js/main.js
+++ js/main.js
@@ -5,7 +5,14 @@ game.keybind(65, 'lefta');
 game.keybind(68, 'rightd');
 game.keybind(87, 'upw');
 game.keybind(83, 'downs');
-game.preload('assets/chara1.png', 'assets/ground.png', 'assets/bground.png', 'assets/cannon.png', 'assets/bullet.png');
+game.preload('assets/chara1.png', 
+			'assets/ground.png', 
+			'assets/bground.png', 
+			'assets/cannon.png', 
+			'assets/bullet.png', 
+			'assets/collideground.wav', 
+			'assets/explode.wav',
+			'assets/hitenemy.wav');
 game.onload = function () {
 	initWorld();
 };
